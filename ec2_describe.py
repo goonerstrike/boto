9b9d3e837cdbf7a3d590c5c92763aa3179dd6a11
root <root@ip-172-31-28-51.us-west-2.compute.internal>
2017-03-08 20:56:26 +0000

    adding tag queries

--- ec2_describe.py
+++ ec2_describe.py
@@ -2,20 +2,21 @@
 import boto3 
 ec2 = boto3.resource('ec2', region_name='us-west-2')
 
-print "\nPrinting Public IP and Instance Name:"
+### Print Running Instances
+print "\nPrinting Public IP and Instance Names of Running Instances:"
 instances = ec2.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
 for instance in instances:
-  print(instance.public_ip_address, instance.tags[0].get("Value"))
-  print instance.tags[0]['Value'], instance.public_ip_address, instance.security_groups[0]['GroupId']
-
-
-print "\n### Printing ID, Public IP and Security Group" 
-simples = ec2.instances.all()
-for simple in simples:
-  print simple.tags[0]['Value'], simple.public_ip_address, simple.security_groups[0]['GroupId']
+#  print(instance.public_ip_address, instance.tags[0].get("Value"))
+  print instance.tags[0]['Value'], "\t", instance.public_ip_address, "\t", instance.security_groups[0]['GroupId']
+  print instance.tags[1].get("Value")
 
 
 
-#print "\nPrinting meta tags"
-#for status in ec2.meta.client.describe_instance_status()['InstanceStatuses': name]:
- #   print status
+
+
+#print "\n\n\n### Printing ID, Public IP and Security Group" 
+#simples = ec2.instances.all()
+#for simple in simples:
+#  print simple.tags[0]['Value'], simple.public_ip_address, simple.security_groups[0]['GroupId']
+
+
